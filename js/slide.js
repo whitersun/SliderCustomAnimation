8adba4a0105df178a6ba88ee029a8bdca14b0073
Sunwoo Jun <37898596+whitersun@users.noreply.github.com>
2023-07-30 16:16:44 +0800

    Add feature

---
+++ js/slide.js
@@ -0,0 +1,187 @@
+import { info } from "./info.js";
+
+const information = info;
+const layoutStart = $('.layoutStart');
+const layoutEnd   = $('.layoutEnd');
+
+const slideStart = layoutStart.find('.slideWrapper');
+const slideEnd = layoutEnd.find('.slideWrapper');
+
+
+function generateHTML() {
+    const generateSlideItem = (index, item) => {
+        return `
+            <div class="slide-item" data-slide="${index}">
+                <div class="dp-content">
+                    <div class="job">
+                        <div class="jobItem">${item.job}</div>
+                        <div class="jobItem">${item.jobItem}</div>
+                    </div>
+                    <div class="name">${item.name}</div>
+                    <div class="quantity">${item.quantity}</div>
+                    <div class="social">${item.social}</div>
+                    <div class="icon">
+                        <img decoding="async" loading="lazy" class="img-fluid" style="filter: invert(1);" src="./assets/icons/facebook.svg" alt="facebook" />
+                        <img decoding="async" loading="lazy" class="img-fluid" style="filter: invert(1);" src="./assets/icons/youtube.svg" alt="youtube" />
+                        <img decoding="async" loading="lazy" class="img-fluid" style="filter: invert(1);" src="./assets/icons/spotify.svg" alt="spotify" />
+                    </div>
+                </div>
+                <div class="dp-img skeleton">
+                    <img
+                        width="900"
+                        height="645"
+                        decoding="async"
+                        loading="lazy"
+                        class="img-fluid" src="https://source.unsplash.com/random/900x645/?japan-temple?${index}"
+                        alt="${item.name}"
+                    />
+                </div>
+            </div>
+        `
+    }
+    
+    const generateSlideItemEnd = (index, item) => {
+        const zIndex = `z-index: ${information.length - index}`;
+        const translateXValues = ['-100', '0', '14']; // Add more values if needed
+        const scaleValues = ['0.8', '1', '0.95', '0.89', '0.85']; // Add more values if needed
+        const translateX = index < translateXValues.length ? translateXValues[index] : index * 9;
+        const scale = index < scaleValues.length ? scaleValues[index] : '0.8';
+        const transform = `transform: translateX(${translateX}%) scale(${scale})`;
+        
+        const style = `${zIndex}; ${transform}`;
+
+        return `
+            <div class="slide-item ${index > 0 ? 'active' : ''}" data-slide="${index}"
+            style="${style}">
+                <div class="view skeleton">
+                    <img 
+                        width="900"
+                        height="645"
+                        decoding="async"
+                        loading="lazy"
+                        class="img-fluid"
+                        src="https://source.unsplash.com/random/900x645/?japan-temple?${index}"
+                        alt="${item.name}"
+                    />
+                </div>
+            </div>
+        `
+    }
+    
+    information.forEach(function (item, index) {
+        slideStart.append(generateSlideItem(index, item));
+        slideEnd.append(generateSlideItemEnd(index, item));
+    });
+}
+
+
+// TODO: Lazy load images
+function LazyLoadingImage () {
+    function loadImage(src) {
+        return new Promise((resolve, reject) => {
+            const image = new Image();
+            image.src = src;
+    
+            image.onload = () => {
+                resolve(image);
+            };
+    
+            image.onerror = () => {
+                reject(new Error("Failed to load image"));
+            };
+        });
+    }
+
+    // Usage example:
+    const imagePromises = slideStart.find('.slide-item').map(function (index, item) {
+        return loadImage(`https://source.unsplash.com/random/900x645/?japan-temple?${index}`)
+            .then((image) => {
+                console.log("Image loaded successfully");
+                // Do something with the loaded image
+                $('.skeleton').removeClass('skeleton');
+            })
+            .catch((error) => {
+                console.error(error);
+                // Handle error condition
+            });
+    })
+
+    Promise.all(imagePromises).then(() => {
+        console.log("All images loaded successfully");
+        // Run your final action here after all images are loaded
+
+        slideStart.find('.slide-item[data-slide="0"]').addClass('active');
+    }).catch((error) => {
+        console.error("Error loading images:", error);
+        // Handle error condition
+    });
+}
+
+
+function cloneSlideItem () {
+    const initialSlide = slideEnd.find('.slide-item[data-slide="0"]').clone().attr({'style': 'transform: translateX(48%) scale(0.79)', 'data-slide': '6'}).removeClass('active');
+    slideEnd.append(initialSlide);
+}
+
+generateHTML();
+// cloneSlideItem();
+LazyLoadingImage();
+
+
+let increasedIndex = 0;
+function ClickToChangeNextSlide (event) {
+    event.preventDefault();
+
+    const prevSlide = slideStart.find(`.slide-item[data-slide="${increasedIndex}"]`);
+    prevSlide.css('position', 'absolute');
+    
+    increasedIndex += 1;
+    if (increasedIndex > information.length - 1) {
+        increasedIndex = 0;
+    }
+
+
+    const slideEndItems = slideEnd.find('.slide-item');
+    slideEnd.find('.slide-item').each(function (index, item) {
+        slideEnd.find('.slide-item').eq(increasedIndex).removeClass('active');
+
+
+
+        const i = index;
+        // TODO if update current index increased by 1
+        index = index + increasedIndex;
+        const conditional = index >= slideEndItems.length ? Math.abs(slideEndItems.length - index) : index;
+        
+        slideEndItems.eq(conditional).addClass('active');
+
+        const zIndex = `z-index: ${slideEndItems.length - i}`;
+        const translateXValues = ['-100', '0', '14']; // Add more values if needed
+        const translateX = i < translateXValues.length ? translateXValues[(i)] : (i) * 9;
+        
+        const scaleValues = ['1', '1', '0.95', '0.89', '0.85']; // Add more values if needed
+        const scale = i < scaleValues.length ? scaleValues[(i)] : '0.8';
+        const transform = `transform: translateX(${translateX}%) scale(${scale})`;
+        
+        const style = `${zIndex}; ${transform}`;
+        
+        slideEnd.find(`.slide-item[data-slide="${conditional}"]`).attr('style', style);
+    });
+
+
+
+
+    
+    const nextSlide = slideStart.find(`.slide-item[data-slide="${increasedIndex}"]`);
+    nextSlide.addClass('active').animate({ position: 'relative', 'z-index': '1' }, function () {
+        prevSlide.css('z-index', '0');
+        setTimeout(() => {
+            prevSlide.removeClass('active');
+
+            $(".slide-item").on("click", ClickToChangeNextSlide);
+        }, 1000);
+    });
+
+    $(".slide-item").off("click", ClickToChangeNextSlide);
+}
+
+$(".slide-item").on("click", ClickToChangeNextSlide);
---
+++ script.js
@@ -0,0 +1,108 @@
+var arr = [];
+
+// Touch
+const carousel = document.querySelector(".carousel");
+const firstImg = carousel.querySelectorAll("img")[0];
+console.log('firstImg: ', firstImg);
+const arrowIcons = document.querySelectorAll(".wrapper i");
+
+let isDragStart = false
+let isDragging = false
+let prevPageX
+let prevScrollLeft
+let positionDiff;
+
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth;
+    if(screen.width == 375)
+        firstImgWidth = firstImg.clientWidth + 20;
+    else
+        firstImgWidth = firstImg.clientWidth + 10;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+
+const dragStart = (e) => {
+    // updating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+
+$("#dp-slider .dp_item").on("click", function () {
+    if ($(this).index() > 0) {
+        $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn(function () {
+            console.log($(this));
+
+            setTimeout(() => {
+                $.each($('.dp_item'), function (index, dp_item) {
+                    $(dp_item).attr('data-position', index + 1);
+                    // 
+                    var get_slide = $(this).attr('data-class');
+                    get_slide = Number(get_slide);
+                    var imgPath = "./assets/images/S" + Number(get_slide) + ".jpg";
+                    $('.img-fluid-default').attr('src', imgPath)
+                });
+            }, 1000);
+        });
+    }
+
+    // const $this = $(this);
+
+    // $("#dp-slider .dp_item:first-child").hide().appendTo("#dp-slider").fadeIn();
+
+    // $.each($('.dp_item'), function (index, dp_item) {
+    //     $(dp_item).attr('data-position', index + 1);
+    //     // 
+    //     var get_slide = $(this).attr('data-class');
+    //     get_slide = Number(get_slide);
+    //     var imgPath = "./assets/images/S" + Number(get_slide) + ".jpg";
+    //     $('.img-fluid-default').attr('src', imgPath)
+    // });
+});
