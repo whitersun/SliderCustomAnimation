f0a3562692212723cc83ee8441ead76f5e84d3d9
Sunwoo Jun <37898596+whitersun@users.noreply.github.com>
2024-03-29 08:48:02 +0800

    update

--- initData.js
+++ initData.js
@@ -2,7 +2,10 @@ const fetchData = async () => {
     const response = await fetch('./data.json');
     const data = await response.json();
 
-    localStorage.setItem('translationsData', JSON.stringify(data));
+    const getTranslateDate = localStorage.getItem('translationsData');
+
+    if (getTranslateDate) return;
+    else localStorage.setItem('translationsData', JSON.stringify(data));
 
     return data;
 }
